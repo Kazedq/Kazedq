5798d23d0230194eeff5f9e7df05ac92e72cc613
Rafsan Kagazuki <kagazukirafsan.com>
2022-11-30 13:25:36 +0700

    upgrade4

--- dbhotelmar.sql
+++ dbhotelmar.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2022 at 04:41 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Waktu pembuatan: 30 Nov 2022 pada 07.23
+-- Versi server: 10.4.25-MariaDB
+-- Versi PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,107 +18,108 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_ukk_2022`
+-- Database: `hotel`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fasilitas_kamar`
+-- Struktur dari tabel `fasilitas_kamar`
 --
 
 CREATE TABLE `fasilitas_kamar` (
   `id_fasilitas` int(11) NOT NULL,
-  `id_kamar` int(11) NOT NULL,
+  `id_kamar` varchar(1000) DEFAULT NULL,
   `fasilitas` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `fasilitas_kamar`
+-- Dumping data untuk tabel `fasilitas_kamar`
 --
 
 INSERT INTO `fasilitas_kamar` (`id_fasilitas`, `id_kamar`, `fasilitas`) VALUES
-(1, 2, 'Kamar Mandi di Dalam\r\nFull AC\r\nKolam Renang'),
-(3, 3, 'Ruang tamu, Kursi Kamar, Kamar Mandi di Dalam'),
-(4, 6, 'Kasur Tidur, Kursi Tamu, Meja Makan');
+(1, '3', 'Kamar Mandi di Dalam\r\nFull AC\r\nKolam Renang'),
+(3, '6', 'Ruang tamu, Kursi Kamar, Kamar Mandi di Dalam');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `galeri`
+-- Struktur dari tabel `galeri`
 --
 
 CREATE TABLE `galeri` (
   `id_galeri` int(11) NOT NULL,
   `keterangan` varchar(255) NOT NULL,
-  `foto` varchar(255) NOT NULL
+  `foto` varchar(255) NOT NULL,
+  `ket_fasilitas` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `galeri`
+-- Dumping data untuk tabel `galeri`
 --
 
-INSERT INTO `galeri` (`id_galeri`, `keterangan`, `foto`) VALUES
-(1, 'Kolam Renang', '398-823c97c0d94e6b570a893e38e23c962e_crop_920x530.jpg'),
-(2, 'Tempat Tidur', '448-5f081b41cc76c.jpeg'),
-(5, 'Lobi', '417-Rayaburi_Hotel_Patong_-_Lobby.jpg'),
-(6, 'Tempat Makan', '295-fairmont.jpg'),
-(7, 'Gedung Hotel', '313-27677-hotel-tertinggi-di-dunia.jpg'),
-(8, 'Resepsionis', '693-Resepsionis.jpg');
+INSERT INTO `galeri` (`id_galeri`, `keterangan`, `foto`, `ket_fasilitas`) VALUES
+(1, 'Kolam Renang', '398-823c97c0d94e6b570a893e38e23c962e_crop_920x530.jpg', NULL),
+(2, 'Tempat Tidur', '448-5f081b41cc76c.jpeg', NULL),
+(5, 'Lobi', '417-Rayaburi_Hotel_Patong_-_Lobby.jpg', NULL),
+(6, 'Tempat Makan', '295-fairmont.jpg', NULL),
+(7, 'Gedung Hotel', '313-27677-hotel-tertinggi-di-dunia.jpg', NULL),
+(8, 'Resepsionis', '693-Resepsionis.jpg', 'loool');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `kamar`
+-- Struktur dari tabel `kamar`
 --
 
 CREATE TABLE `kamar` (
   `id_kamar` int(11) NOT NULL,
-  `no_kamar` varchar(255) DEFAULT NULL,
+  `tipe_kamar` varchar(255) DEFAULT NULL,
   `foto` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `kamar`
+-- Dumping data untuk tabel `kamar`
 --
 
-INSERT INTO `kamar` (`id_kamar`, `no_kamar`, `foto`) VALUES
-(2, 'Kamar No. 1', '284-slide2.png'),
-(3, 'Kamar No. 2', '776-slide__.png'),
-(6, 'Kamar No. 3', '377-5f081b41cc76c.jpeg');
+INSERT INTO `kamar` (`id_kamar`, `tipe_kamar`, `foto`) VALUES
+(3, 'Superior', '776-slide__.png'),
+(6, 'Delux', '377-5f081b41cc76c.jpeg');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pesanan`
+-- Struktur dari tabel `pesanan`
 --
 
 CREATE TABLE `pesanan` (
   `id_pesanan` int(11) NOT NULL,
   `cek_in` varchar(255) DEFAULT NULL,
   `cek_out` varchar(255) DEFAULT NULL,
+  `tipe_kamar` varchar(1000) DEFAULT NULL,
   `jml_kamar` varchar(255) DEFAULT NULL,
   `nama_pemesan` varchar(255) DEFAULT NULL,
   `email_pemesan` varchar(255) DEFAULT NULL,
   `hp_pemesan` varchar(255) DEFAULT NULL,
   `nama_tamu` varchar(255) DEFAULT NULL,
   `id_kamar` int(11) DEFAULT NULL,
-  `status` varchar(2) DEFAULT NULL
+  `status` varchar(2) DEFAULT NULL,
+  `kode_reservasi` varchar(1000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `pesanan`
+-- Dumping data untuk tabel `pesanan`
 --
 
-INSERT INTO `pesanan` (`id_pesanan`, `cek_in`, `cek_out`, `jml_kamar`, `nama_pemesan`, `email_pemesan`, `hp_pemesan`, `nama_tamu`, `id_kamar`, `status`) VALUES
-(2, '2022-03-24', '2022-03-25', '1', 'Mardeta', 'mardeta@gmail.com', '0075664', 'Mardeta', 3, '2'),
-(3, '2022-03-30', '2022-03-31', '1', 'Diandra', 'diandra@gmail.com', '0075664', 'Diandra', 2, '2'),
-(4, '2022-03-28', '2022-03-29', '1', 'Jumsih', 'jumsih@gmail.com', '070600', 'Jumsih', 6, '2');
+INSERT INTO `pesanan` (`id_pesanan`, `cek_in`, `cek_out`, `tipe_kamar`, `jml_kamar`, `nama_pemesan`, `email_pemesan`, `hp_pemesan`, `nama_tamu`, `id_kamar`, `status`, `kode_reservasi`) VALUES
+(4, '2022-03-28', '2022-03-29', NULL, '1', 'Jumsih', 'jumsih@gmail.com', '070600', 'Jumsih', 6, '2', NULL),
+(5, '2001-11-11', '2118-01-22', 'Superior', '1', 'sep', 'dwi@gmail.com', '12132313', 'pak dedi', 3, '1', 'EqnQBXSDMw'),
+(6, '2001-11-11', '2118-01-22', 'Superior', '1', 'sep', 'dwi@gmail.com', '12132313', 'pak dedi', 3, '1', 'KI4MxordsV');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -130,7 +131,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `nama`, `username`, `password`, `level`) VALUES
@@ -142,65 +143,65 @@ INSERT INTO `users` (`id`, `nama`, `username`, `password`, `level`) VALUES
 --
 
 --
--- Indexes for table `fasilitas_kamar`
+-- Indeks untuk tabel `fasilitas_kamar`
 --
 ALTER TABLE `fasilitas_kamar`
   ADD PRIMARY KEY (`id_fasilitas`);
 
 --
--- Indexes for table `galeri`
+-- Indeks untuk tabel `galeri`
 --
 ALTER TABLE `galeri`
   ADD PRIMARY KEY (`id_galeri`);
 
 --
--- Indexes for table `kamar`
+-- Indeks untuk tabel `kamar`
 --
 ALTER TABLE `kamar`
   ADD PRIMARY KEY (`id_kamar`);
 
 --
--- Indexes for table `pesanan`
+-- Indeks untuk tabel `pesanan`
 --
 ALTER TABLE `pesanan`
   ADD PRIMARY KEY (`id_pesanan`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `fasilitas_kamar`
+-- AUTO_INCREMENT untuk tabel `fasilitas_kamar`
 --
 ALTER TABLE `fasilitas_kamar`
   MODIFY `id_fasilitas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `galeri`
+-- AUTO_INCREMENT untuk tabel `galeri`
 --
 ALTER TABLE `galeri`
   MODIFY `id_galeri` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- AUTO_INCREMENT for table `kamar`
+-- AUTO_INCREMENT untuk tabel `kamar`
 --
 ALTER TABLE `kamar`
   MODIFY `id_kamar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `pesanan`
+-- AUTO_INCREMENT untuk tabel `pesanan`
 --
 ALTER TABLE `pesanan`
-  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pesanan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
